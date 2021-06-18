86f8ab9fc0fa5d41ab005a70e85952af7894e69b
ddPn08 <pyn.g.1125@gmail.com>
2021-06-18 19:00:41 +0900

    Some fixes

--- Bukkit/src/main/java/world/ddpn/bungeecordlinkersportal/commands/LinkersPortal.java
+++ Bukkit/src/main/java/world/ddpn/bungeecordlinkersportal/commands/LinkersPortal.java
@@ -3,8 +3,6 @@ package world.ddpn.bungeecordlinkersportal.commands;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.PlainDocument;
-
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
